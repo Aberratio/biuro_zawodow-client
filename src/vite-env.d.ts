9cd4b6faaf533688321509a85efe994311d980d5
Aberratio <joannakokot97@gmail.com>
2026-05-27 22:00:51 +0200

    Extend legal pages content

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,9 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_API_URL?: string;
   readonly VITE_MOUSEFLOW_PROJECT_ID?: string;
   readonly VITE_MOUSEFLOW_ENABLED?: string;
-  readonly VITE_LEGAL_ADMIN_NAME?: string;
-  readonly VITE_LEGAL_ADMIN_ADDRESS?: string;
-  readonly VITE_LEGAL_CONTACT_EMAIL?: string;
 }
 
 interface ImportMeta {
